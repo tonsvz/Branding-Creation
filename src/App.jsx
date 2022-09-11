3dff4dbdf46422a58c4d0ff6f6659a8b808ac4d8
Tony Veliz <tonsvz@gmail.com>
2022-09-10 21:06:49 -0500

    static images done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from "react";
-import Banner from "./components/Banner";
-import Cases from "./components/Cases";
-import Header from "./components/Header";
-import "./styles/App.scss";
+import React, { useEffect } from 'react';
+import Banner from './components/Banner';
+import Cases from './components/Cases';
+import Header from './components/Header';
+import Test from './components/Test';
+import './styles/App.scss';
 
 const App = () => {
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }, []);
   return (
     <div>
       <Header />
       <Banner />
       <Cases />
+      <Test />
     </div>
   );
 };
---
+++ src/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div>
+      <div className='container'>
+        {/* <div className='img'>
+          <img src='/assets/curology-min.png' alt='' />
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Test;
